dcd4ec28741df1c2472defa74decbb702634c746
erick <ericktiepo337@gmail.com>
2026-08-19 13:01:17 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class Program {
@@ -35,5 +36,9 @@ public class Program {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.now(), 4000.0, department);        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id == "+ newSeller.getId());
     }
 }
